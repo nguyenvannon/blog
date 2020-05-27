ecbab383f735e1eeec191a6d3032fd27940b694d
Non Nguyen <37391219+nguyenvannon@users.noreply.github.com>
2020-05-27 20:48:44 +0700

    Create custom-dialog-flutter.dart

---
+++ custom-dialog-flutter.dart
@@ -0,0 +1,42 @@
+// Hàm khởi tạo hộp thoại cảnh báo có nút
+  void _showAlert(BuildContext context, String title, String content) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+                borderRadius:
+                BorderRadius.circular(20.0)),
+            child: Container(
+              height: 200,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Họ và tên của bạn là gì?'),
+                    ),
+                    SizedBox(
+                      width: 320.0,
+                      child: RaisedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Chào tôi cái nào",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        color: Colors.blue,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
+  }
+  
+  // Ví dụ gọi hàm:
+  _showAlert(context, 'Thông báo', 'Ví dụ này được demo bởi Non Nguyen Blog!');
